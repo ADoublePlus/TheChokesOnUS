69e7ec3155b50a9dd6cba6e3e22c88e57f58bfac
John <jmpwonson@gmail.com>
2019-10-08 12:39:23 +1100

    Assessment_ProgressPoint1
    Added a conveyor belt which moves an object towards an endpoint.

---
+++ RapidGamePrototyping_Assessment2/Assets/ConveyorBelt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public GameObject belt;
+    public Transform endPoint;
+    public float speed;
+
+    void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endPoint.position, speed * Time.deltaTime);
+    }
+}
