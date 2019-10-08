674f2c46325eaf91158cfe6d9c04cd16beb725b9
John <jmpwonson@gmail.com>
2019-10-08 18:45:23 +1100

    Testing
    Tested the collision between two objects.
    TODO: Soundclip needs to be inputted

---
+++ RapidGamePrototyping_Assessment2/Assets/Scripts/BinTune.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinTune : MonoBehaviour
+{
+    private AudioSource audio;
+    AudioClip trashBang;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bin")
+        {
+            StartCoroutine(Ding());
+        }
+    }
+
+    IEnumerator Ding()
+    {
+        gameObject.SetActive(false);
+        audio.PlayOneShot(trashBang);
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
